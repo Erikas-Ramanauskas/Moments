cc56cf88b26fc474c03142ca9141fe8b4c289965
Chaosas100 <101866007+Erikas-Ramanauskas@users.noreply.github.com>
2024-04-14 22:15:46 +0100

    post changes

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://django-rest-testing-904eb712a024.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
